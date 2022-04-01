fa4619499988d46a0575553dd65e6d2e34055618
Ttibsi <ashisbitt@icloud.com>
2022-04-01 14:38:53 +0100

    slight update to zsh aliases

--- templates/zsh-shell/.oh-my-zsh/custom/alias.zsh
+++ templates/zsh-shell/.oh-my-zsh/custom/alias.zsh
@@ -2,12 +2,13 @@
 alias la="ls -Alh"
 
 #Git 
-alias gitgraph="git log --oneline --graph --decorate --all"
+alias gitgraph="git log --oneline --graph --decorate"
 alias ghb="gh browse"
 alias gbl="git branch --list"
 alias gss="git status -s"
-alias gst="git status "
-alias gau="git add -u "
+alias gst="git status"
+alias gau="git add -u"
+alias ga.="git add ."
 alias gph="git push origin HEAD"
 
 # Other
