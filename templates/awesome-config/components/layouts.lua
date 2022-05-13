6333f50d356993aabf0b167cd589964343f86deb
Ttibsi <ashisbitt@icloud.com>
2022-05-13 22:10:18 +0100

    Split the default RC into custom blocks

---
+++ templates/awesome-config/components/layouts.lua
@@ -0,0 +1,9 @@
+local awful = require("awful")
+
+-- Table of layouts to cover with awful.layout.inc, order matters.
+awful.layout.layouts = {
+	awful.layout.suit.tile,
+	awful.layout.suit.floating,
+	awful.layout.suit.max.fullscreen,
+}
+-- }}}
