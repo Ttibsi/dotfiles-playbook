9e8af2ebba11418b25375c7c3a9b7cfd2de5d61e
Ttibsi <ashisbitt@icloud.com>
2022-05-17 09:26:09 +0100

    Customise theme

--- templates/awesome-config/components/layouts.lua
+++ templates/awesome-config/components/layouts.lua
@@ -2,7 +2,7 @@ local awful = require("awful")
 
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
-	awful.layout.suit.tile,
+	awful.layout.suit.tile.left,
 	awful.layout.suit.floating,
 	awful.layout.suit.max.fullscreen,
 }
--- templates/awesome-config/config/theme.lua
+++ templates/awesome-config/config/theme.lua
@@ -11,7 +11,7 @@ local themes_path = gfs.get_themes_dir()
 
 local theme = {}
 
-theme.font = "sans 8"
+theme.font = "DroidSansMono Nerd Font Mono 10"
 
 theme.bg_normal = "#222222"
 theme.bg_focus = "#535d6c"
@@ -24,10 +24,10 @@ theme.fg_focus = "#ffffff"
 theme.fg_urgent = "#ffffff"
 theme.fg_minimize = "#ffffff"
 
-theme.useless_gap = dpi(0)
+theme.useless_gap = dpi(5)
 theme.border_width = dpi(1)
-theme.border_normal = "#000000"
-theme.border_focus = "#535d6c"
+theme.border_normal = "#ffffff"
+theme.border_focus = "#ffffff"
 theme.border_marked = "#91231c"
 
 -- There are other variable sets
