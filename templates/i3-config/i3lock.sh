365e5ce35ffe8121f3809d7467fccfdf8e1898fa
Ttibsi <ashisbitt@icloud.com>
2022-05-27 15:58:43 +0100

    I3lock with most config set up

--- templates/i3-config/i3lock.sh
+++ templates/i3-config/i3lock.sh
@@ -1,36 +1,47 @@
 #!/bin/sh
 
-BLANK='#00000000'
-CLEAR='#ffffff22'
-DEFAULT='#ff00ffcc'
-TEXT='#ee00eeee'
-WRONG='#880000bb'
-VERIFYING='#bb00bbbb'
+WHITE='#ffffffff'
+LOCK_COL='#77777766'
+WRONG='#88000000'
+RAND_IMAGE=$(find ~/.wallpapers -name "*.jpg" | shuf n1)
 
 i3lock \
---insidever-color=$CLEAR     \
---ringver-color=$VERIFYING   \
+--image=$RAND_IMAGE          \
+--blur 10                    \
 \
---insidewrong-color=$CLEAR   \
+--indicator \
+--radius=10 \
+--ring-width=5 \
+--ind-pos="x+w/15-30:y+h/15"   \
+--ring-color=$WHITE         \
+--ringver-color=$WHITE       \
 --ringwrong-color=$WRONG     \
 \
---inside-color=$BLANK        \
---ring-color=$DEFAULT        \
---line-color=$BLANK          \
---separator-color=$DEFAULT   \
+--verif-text="" \
+--wrong-text="" \
+--verif-color=$WHITE          \
+--verif-size=25 \
+--wrong-size=25 \
+--wrong-color=$WHITE          \
 \
---verif-color=$TEXT          \
---wrong-color=$TEXT          \
---time-color=$TEXT           \
---date-color=$TEXT           \
---layout-color=$TEXT         \
---keyhl-color=$WRONG         \
---bshl-color=$WRONG          \
-\
---screen 1                   \
---blur 5                     \
---clock                      \
---indicator                  \
+--clock \
 --time-str="%H:%M:%S"        \
---date-str="%A, %Y-%m-%d"       \
---keylayout 1                \
+--date-str="%A %d-%m-%Y"       \
+--time-size=40 \
+--date-size=25 \
+--time-font="DroidSansMono Nerd Font Mono" \
+--date-font="DroidSansMono Nerd Font Mono" \
+--time-color=$WHITE           \
+--date-color=$WHITE           \
+--time-pos="w/2:h/2-25" \
+--date-pos="w/2:h/2+15" \
+\
+--beep                       \
+--ignore-empty-password \
+\
+--greeter-text=""          \
+--greeter-size=180            \
+--greeter-color=$LOCK_COL     \
+--greeteroutline-color=$WHITE \
+--greeter-font="DroidSansMono Nerd Font Mono" \
+--greeter-pos=w/2:h/3
