547edbe2d7a87764ab02b90c12f56f3b398ec23f
Ttibsi <ashisbitt@icloud.com>
2023-03-15 15:09:39 +0000

    telescope find hidden files

--- templates/nvim/lua/telescope_config.lua
+++ templates/nvim/lua/telescope_config.lua
@@ -1,9 +1,37 @@
 local function init()
+	local telescope = require("telescope")
 	local builtin = require("telescope.builtin")
+	local telescopeConfig = require("telescope.config")
+
 	vim.keymap.set("n", "<leader>f", builtin.find_files, {})
 	vim.keymap.set("n", "<leader>g", builtin.live_grep, {})
 	vim.keymap.set("n", "<leader>b", builtin.buffers, {})
 	vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+	local vimgrep_arguments =
+		{ unpack(telescopeConfig.values.vimgrep_arguments) }
+	table.insert(vimgrep_arguments, "--hidden")
+	table.insert(vimgrep_arguments, "--glob")
+	table.insert(vimgrep_arguments, "!**/.git/*")
+
+	telescope.setup({
+		defaults = {
+			-- `hidden = true` is not supported in text grep commands.
+			vimgrep_arguments = vimgrep_arguments,
+		},
+		pickers = {
+			find_files = {
+				-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+				find_command = {
+					"rg",
+					"--files",
+					"--hidden",
+					"--glob",
+					"!**/.git/*",
+				},
+			},
+		},
+	})
 end
 
 return {
