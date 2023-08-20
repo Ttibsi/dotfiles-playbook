d2e8deb4860efa67f5be45d333b2fc709c2b5fb4
ttibsi <ashisbitt@icloud.com>
2023-08-20 16:11:47 +0100

    first round of updates from testing

--- dotfiles.sh
+++ dotfiles.sh
@@ -15,71 +15,75 @@ function gnome {
 
 function i3 {
     echo -e "Configuring i3"
-    ansible-playbook -K tasks/i3/yml
+    ansible-playbook -K tasks/i3.yml
 }
 
-# Check out correct branch
-if ! [ -z $1 ]; then
-    echo ----- Checking out $1 -----
-    git checkout -b $1 origin/$1
-fi
+function main {
+    # Check out correct branch
+    if ! [ -z $1 ]; then
+        echo ----- Checking out $1 -----
+        git checkout -b $1 origin/$1
+    fi
 
-echo "Select a Desktop Environment to configure (or empty for all)"
-echo "1 - GNOME"
-echo "2 - i3"
+    echo "Select a Desktop Environment to configure (or empty for all)"
+    echo "1 - GNOME"
+    echo "2 - i3"
 
-read -p "> " de_choice
+    read -p "> " de_choice
 
-sudo dpkg --configure --pending
-sudo apt-get update 
-sudo apt-get upgrade -y
+    sudo dpkg --configure --pending
+    sudo apt-get update 
+    sudo apt-get upgrade -y
 
-# Check if ansible is installed
-if ! [ -x "$(command -v ansible)" ]; then
-    echo -e  "\n----- Installing ansible -----"
-    sudo apt install ansible -y
-fi
+    # Check if ansible is installed
+    if ! [ -x "$(command -v ansible)" ]; then
+        echo -e  "\n----- Installing ansible -----"
+        sudo apt install ansible -y
+    fi
 
-# Generate SSH keys
-if ! [[ -f "$SSH_DIR/authorized_keys" ]]; then
-    echo -e "\n----- Generating SSH key -----"
-    mkdir -p "$SSH_DIR"
-    chmod 700 "$SSH_DIR"
-    ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "$USER@$HOSTNAME"
-    cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
-fi
+    # Generate SSH keys
+    if ! [[ -f "$SSH_DIR/authorized_keys" ]]; then
+        echo -e "\n----- Generating SSH key -----"
+        mkdir -p "$SSH_DIR"
+        chmod 700 "$SSH_DIR"
+        ssh-keygen -b 4096 -t rsa -f "$SSH_DIR/id_rsa" -N "" -C "$USER@$HOSTNAME"
+        cat "$SSH_DIR/id_rsa.pub" >> "$SSH_DIR/authorized_keys"
+    fi
 
-# Install ansible requirements
-echo -e "\n----- Installing requirements -----"
-ansible-galaxy install -r requirements.yml
+    # Install ansible requirements
+    echo -e "\n----- Installing requirements -----"
+    ansible-galaxy install -r requirements.yml
 
-ansible-playbook -K main.yml
+    ansible-playbook -K main.yml
 
-case "$de_choice" in 
-    1)
-        gnome
-        ;;
-    2)
-        i3
-        ;;
-    *)
-        gnome
-        i3
-        ;;
-esac
+    case "$de_choice" in 
+        1)
+            gnome
+            ;;
+        2)
+            i3
+            ;;
+        *)
+            gnome
+            i3
+            ;;
+    esac
 
-echo -e "\n----- Removing ansible requirements -----"
-rm -rf $HOME/.ansible
+    echo -e "\n----- Removing ansible requirements -----"
+    rm -rf $HOME/.ansible
 
-echo -e "\n----- Cleaning up apt -----"
-sudo apt autoclean -y
-sudo apt autoremove -y
+    echo -e "\n----- Cleaning up apt -----"
+    sudo apt autoclean -y
+    sudo apt autoremove -y
 
-echo -e "\n----- Copying SHH pub key to clipboard -----"
-cat "$SSH_DIR/id_rsa.pub" | xclip -selection c
-echo Add to account here: https://github.com/settings/keys
+    echo -e "\n----- Copying SHH pub key to clipboard -----"
+    cat "$SSH_DIR/id_rsa.pub" | xclip -selection c
+    echo Add to account here: https://github.com/settings/keys
 
-echo -e "\n---------------\n"
+    echo -e "\n---------------\n"
 
-echo -e "Provisioning complete"
-echo -e "Reboot the system now"
+    echo -e "Provisioning complete"
+    echo -e "Reboot the system now"
+}
+
+time main
